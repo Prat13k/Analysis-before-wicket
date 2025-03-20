8bc66665109ffc971a11c3bf49cb0c2f713cf3cd
Pratik Patil <ppratik1399@gmail.com>
2025-03-20 15:18:58 +0530

    almost final update

--- app.py
+++ app.py
@@ -1,35 +1,34 @@
 from flask import Flask, render_template, request, redirect, url_for
 from database_connection import get_db_connection
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder = 'templates')
 
 @app.route('/')
 def main():
     db = get_db_connection()
     cursor = db.cursor(dictionary = True)
-    cursor.execute("SELECT blog_title, blog_content FROM blog_details")
+    cursor.execute("SELECT blog_title, blog_content FROM blog_details ORDER BY blog_publish_date DESC")
     blog_details = cursor.fetchall()
     db.close()
     return render_template("main.html", blog_details = blog_details)
 
-@app.route('/about-us')
-def about():
-    return render_template("aboutus.html", blog_details = blog_details)
+@app.route('/aboutus.html')
+def aboutus():
+    return render_template("aboutus.html")
 
 @app.route('/subscribe',methods = ['POST'])
-def subscribe(): 
+def subscribe():
+    print(request.form) 
     email = request.form['email']
     db = get_db_connection()
     cursor = db.cursor()
     try:
-        cursor.execute('INSERT INTO subscriber_email values($s)',(email,))
+        cursor.execute('INSERT INTO subscriber_list(subscriber_email) VALUES (%s)',(email,))
         db.commit()
-        db.close()
-
     except Exception as e:
         print(f'Error: {e}')
-        
     finally:
+        cursor.close()
         db.close() 
          
     return redirect(url_for('main'))
