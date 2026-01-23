b2443c7051405b431b873ed5a5c3cdc4d2a87603
Pratik Patil <81090410+Prat13k@users.noreply.github.com>
2026-01-23 19:35:31 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://dzesefiyvtsidcpmpocm.supabase.co;
+const SUPABASE_URL = "https://dzesefiyvtsidcpmpocm.supabase.co";
 const SUPABASE_KEY = "sb_publishable_34fBGpJHD1fydYVcGjIGDg_bwfz6X15"
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
