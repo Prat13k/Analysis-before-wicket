6c1f1d0473a193137a340b2d6fd499dd160ec5d2
Pratik Patil <81090410+Prat13k@users.noreply.github.com>
2026-01-29 13:18:45 +0530

    Update index.page.js

--- js/index.page.js
+++ js/index.page.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", async () => {
                 <a href="blog.html?slug=${blog.slug}" class="blog-link">
                     <article class="blog-card">
                         <h2>${blog.title}</h2>
-                        <p>${blog.content.substring(0, 200)}...</p>
+                        <p>${blog.content.substring(0, 100)}...</p>
                         <small>${new Date(blog.created_at).toDateString()}</small>
                     </article>
                 </a>`
